8d05ac068f3c9a51e4687a67caffc44c08da8ecd
pcaponetti <paul.caponetti@gmail.com>
2017-09-13 15:49:36 -0400

    wip

--- src/store/mqtt/actions.ts
+++ src/store/mqtt/actions.ts
@@ -1,42 +1,28 @@
-import init from 'react_native_mqtt';
-import { AsyncStorage } from 'react-native';
-import * as redux from 'redux';
+import * as mqtt from 'mqtt';
+import { Dispatch as reduxDispatch } from 'redux';
 import { cloneDeep, values } from 'lodash';
 import xively from '../../lib/xively';
-import { Device, AppState } from '../../types';
+import { Device, Channel, AppState } from '../../types';
 import { constants, MqttState, serdes, ActionRestoreSubscriptionState } from './reducers';
 import { parseTopic } from './utils';
 import { ChannelType, MqttMessage, TopicData } from './types';
 
 // TODO use a config param
-const MQTT_HOST = 'broker.xively.com';
+const MQTT_HOST = 'wss://broker.xively.com:443/';
 
-let client: Paho.MQTT.Client;
+let client: mqtt.Client; // lsl
 
-let handlers: { [eventName: string]: any } = {};
+let handlers = {};
 
 type GetState = () => AppState;
-type Dispatch = redux.Dispatch<AppState>;
+type Dispatch = reduxDispatch<AppState>;
 
 export function connect() {
   return async function(dispatch: Dispatch, getState: GetState) {
-
-// init MQTT storage
-    init({
-      size: 10000,
-      storageBackend: AsyncStorage,
-      defaultExpires: 1000 * 3600 * 24,
-      enableCache: true,
-      sync: {
-      }
-    });
-
     const state = getState();
-
-    // TODO: paho doesn't seem to have this status field
-    // if (client || state.mqtt.connected) {
-    //   throw new Error(`MQTT: attempting to connect while already connected`);
-    // }
+    if (client || state.mqtt.connected) {
+      throw new Error(`MQTT: attempting to connect while already connected`);
+    }
 
     let jwt;
     try {
@@ -48,9 +34,18 @@ export function connect() {
       return;
     }
 
-    client = new Paho.MQTT.Client(MQTT_HOST, 443, '', '');
+    client = mqtt.connect(MQTT_HOST, {
+      username: 'Auth:JWT',
+      password: jwt,
+    });
 
     dispatch(connecting());
+
+    handlers['connect'] = () => dispatch({ type: constants.CONNECTED });
+    handlers['close'] = () => dispatch(notConnected());
+    handlers['offline'] = () => dispatch(notConnected());
+    handlers['error'] = (err: any) => dispatch(setError(`Something went wrong: ${err}`));
+    handlers['message'] = (topic: string, message: MqttMessage) => dispatch(onMessage(topic, message));
 
     // This is one of the most complex things in the code,
     // the main purpose of this logic is to try to auto-reconnect after disconnecting.
@@ -62,52 +57,31 @@ export function connect() {
     // Probably this whole thing would benefit from some refactor if you are up for it.
     // TODO:
     // - crate hooks so when the user logs out this is stopped
-    const reconnect = async () => {
+    handlers['reconnect'] = async () => {
       dispatch({ type: constants.RECONNECTING });
       // save state.mqtt.data
       const subscriptions = cloneDeep(getState().mqtt.data);
-      // might already be disconnected
-      try {
-        // disconnect
-        await dispatch(disconnect());
-      } catch (err) {
-        console.log('Attempted to disconnect, but already disconnected');
-      }
+      // disconnect
+      await dispatch(disconnect());
       // connect
       await dispatch(connect());
-      try {
-        // re subscribe mqtt
-        client.subscribe(Object.keys(subscriptions), (error, granted) => {
-          if (error) {
-            dispatch(setError(error.message));
-            console.log(`MQTT: something went wrong while subscribing`, error);
-            return;
-          }
-        });
-        // restore state.mqtt.data
-        dispatch(restoreSubscriptionState(subscriptions));
-      } catch (err) {
-        // retry connection
-        console.log('MQTT subscribe failed, trying again in 4 seconds');
-        setTimeout(() => { reconnect(); }, 4000);
-      }
+      // re subscribe mqtt
+      client.subscribe(Object.keys(subscriptions), (error, granted) => {
+        if (error) {
+          dispatch(setError(error.message));
+          console.log(`MQTT: something went wrong while subscribing`, error);
+          return;
+        }
+      });
+      // restore state.mqtt.data
+      dispatch(restoreSubscriptionState(subscriptions));
     };
 
-    handlers['onConnected'] = () => dispatch({ type: constants.CONNECTED });
-    handlers['onConnectionLost'] = reconnect;
-    handlers['onMessageArrived'] = (message: MqttMessage) => dispatch(onMessage(message.destinationName, message.payloadString));
-
-    Object.keys(handlers).forEach((eventName) => {
-      client[eventName] = handlers[eventName];
-    })
-
-    client.connect({
-      userName: 'Auth:JWT',
-      password: jwt,
-      onSuccess: () => dispatch({ type: constants.CONNECTED }),
-      onFailure: (err) => dispatch(setError(`Something went wrong: ${JSON.stringify(err)}`)),
-      useSSL: true,
-    });
+    Object.keys(handlers)
+      .map((eventName) => [eventName, handlers[eventName]])
+      .forEach(([eventName, handler]) => {
+        client.on(eventName, handler);
+      });
   };
 }
 
@@ -119,9 +93,11 @@ export function disconnect() {
       return;
     }
 
-    Object.keys(handlers).forEach((eventName) => {
-      client[eventName] = null;
-    });
+    Object.keys(handlers)
+      .map((eventName) => [eventName, handlers[eventName]])
+      .forEach(([eventName, handler]) => {
+        client.removeListener(eventName, handler);
+      });
 
     client.end(true);
 
@@ -135,7 +111,7 @@ export function subscribeDevice(device: Device) {
   return async function(dispatch: Dispatch, getState: GetState) {
     const state: MqttState = getState().mqtt;
     const subscriptions = values(state.data);
-    const notSubscribedTopics = (channel) => !(subscriptions as any).includes(channel.channel);
+    const notSubscribedTopics = (channel: Channel) => !(subscriptions as any).includes(channel.channel);
 
     const topicsToSubscribe = device.channels
       .filter(notSubscribedTopics)
@@ -143,39 +119,27 @@ export function subscribeDevice(device: Device) {
         topic: channel.channel,
         channelType: channel.persistenceType,
       }));
-      topicsToSubscribe.forEach((topic) => {
-        client.subscribe(topic.topic, {
-          onFailure: (err) => {
-            dispatch(setError(err.errorMessage));
-            console.log(`MQTT: something went wrong while subscribing`, err.errorMessage);
-          },
+
+    client.subscribe(topicsToSubscribe.map((channel) => channel.topic), (error, granted) => {
+      if (error) {
+        dispatch(setError(error.message));
+        console.log(`MQTT: something went wrong while subscribing`, error);
+        return;
+      }
+
+      const failedTopics = granted
+        .filter((grant) => grant.qos === 128)
+        .map((grant) => grant.topic);
+
+      failedTopics.forEach((topic) => {
+          console.error(`MQTT subscription denied for ${topic}`);
         });
-      });
 
-    // TODO: make sure we got the ack on the subscribes
-    dispatch(subscribed(topicsToSubscribe));
+      const successfulySubscribedTopics = topicsToSubscribe
+        .filter((topicConfig) => !(failedTopics as any).includes(topicConfig.topic));
 
-    // TODO: figure out how to mimic this functionality from MQTTJS
-    // client.subscribe(topicsToSubscribe.map((channel) => channel.topic), (error, granted) => {
-    //   if (error) {
-    //     dispatch(setError(error.message));
-    //     console.log(`MQTT: something went wrong while subscribing`, error);
-    //     return;
-    //   }
-
-    //   const failedTopics = granted
-    //     .filter((grant) => grant.qos === 128)
-    //     .map((grant) => grant.topic);
-
-    //   failedTopics.forEach((topic) => {
-    //       console.warn(`MQTT subscription denied for ${topic}`);
-    //     });
-
-    //   const successfulySubscribedTopics = topicsToSubscribe
-    //     .filter((topicConfig) => !(failedTopics as any).includes(topicConfig.topic));
-
-    //   dispatch(subscribed(successfulySubscribedTopics));
-    // });
+      dispatch(subscribed(successfulySubscribedTopics));
+    });
   };
 }
 
@@ -183,7 +147,7 @@ export function unsubscribeDevice(device: Device) {
   return async function(dispatch: Dispatch, getState: GetState) {
     const state: MqttState = getState().mqtt;
     const subscriptions = values(state.data);
-    const subscribedTopics = (channel) => !(subscriptions as any).includes(channel.channel);
+    const subscribedTopics = (channel: Channel) => !(subscriptions as any).includes(channel.channel);
 
     const topicsToUnsubscribe = device.channels.filter(subscribedTopics).map((channel) => channel.channel);
     client.unsubscribe(topicsToUnsubscribe);
@@ -202,14 +166,14 @@ export function unsubscribeAll() {
 }
 
 
-export function onMessage(topic: string, message: string) {
+export function onMessage(topic: string, message: MqttMessage) {
   return async function(dispatch: Dispatch, getState: GetState) {
     const state: MqttState = getState().mqtt;
     const channelType = state.data[topic].channelType;
     const payload = {
       topic,
       channelType,
-      stringPayload: message,
+      stringPayload: message.toString(),
     };
 
 
@@ -221,13 +185,13 @@ export function onMessage(topic: string, message: string) {
 }
 
 
-export function send<T>(topic: string, payload: T, qos: number, retained: boolean) {
+export function send<T>(topic: string, payload: T, options: mqtt.IClientPublishOptions = {}) {
   return async function(dispatch: Dispatch, getState: GetState) {
     let channel;
     try {
       channel = parseTopic(topic).channel;
     } catch (err) {
-      console.warn(`MQTT: bad topic ${topic}, ignoring`);
+      console.error(`MQTT: bad topic ${topic}, ignoring`);
       return;
     }
 
@@ -235,11 +199,15 @@ export function send<T>(topic: string, payload: T, qos: number, retained: boolea
     try {
       stringifier = serdes[channel].stringify;
     } catch (err) {
-      console.warn(`MQTT: stringifier not found for channel ${channel}, ignoring`);
+      console.error(`MQTT: stringifier not found for channel ${channel}, ignoring`);
       return;
     }
 
-    client.publish(topic, stringifier(payload), qos, retained);
+    client.publish(topic, stringifier(payload), options, (err) => {
+      if (err) {
+        dispatch(setError(err.message));
+      }
+    });
   };
 }
 
