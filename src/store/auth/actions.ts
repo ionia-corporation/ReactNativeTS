27789819187af8a5c938042a15e648c2c452d792
Harris Mendell <harrismendell@gmail.com>
2018-02-13 12:06:08 -0500

    create login action creators

--- src/store/auth/actions.ts
+++ src/store/auth/actions.ts
@@ -1,12 +1,50 @@
+import xively from '../../lib/xively';
 
-
+export const AUTH_LOGIN_REQUEST = 'auth/LOGIN_REQUEST';
 export const AUTH_LOGIN_SUCCESS = 'auth/LOGIN_SUCCESS';
+export const AUTH_LOGIN_FAILURE = 'auth/LOGIN_FAILURE';
+export const AUTH_LOGOUT_REQUEST = 'auth/LOGOUT_REQUEST';
 export const AUTH_LOGOUT_SUCCESS = 'auth/LOGOUT_SUCCESS';
+export const AUTH_LOGOUT_FAILURE = 'auth/LOGOUT_FAILURE';
 
-export const loginSuccess = () => ({
+const loginRequest = () => ({
+  type: AUTH_LOGIN_REQUEST,
+})
+
+const loginSuccess = () => ({
   type: AUTH_LOGIN_SUCCESS,
 });
 
-export const logoutSuccess = () => ({
-  type: AUTH_LOGOUT_SUCCESS
+const loginFailure = (error) => ({
+  type: AUTH_LOGIN_FAILURE,
+  payload: error,
+})
+
+const logoutRequest = () => ({
+  type: AUTH_LOGOUT_REQUEST,
+})
+
+const logoutSuccess = () => ({
+  type: AUTH_LOGOUT_SUCCESS,
 });
+
+const logoutFailure = (error) => ({
+  type: AUTH_LOGOUT_FAILURE,
+  payload: error,
+})
+
+
+export const login = (userOptions) => {
+  return async (dispatch) => {
+    try {
+      dispatch(loginRequest());
+      const res = await xively.idm.authentication.login(userOptions);
+      if (!res.jwt) {
+        throw new Error('Not Authorized');
+      }
+      dispatch(loginSuccess());
+    } catch(err) {
+      dispatch(loginFailure(err))
+    }
+  }
+}
--- src/store/auth/reducers.ts
+++ src/store/auth/reducers.ts
@@ -1,22 +1,33 @@
 import * as actions from './actions';
 import { AppState } from '../../types';
 import { User } from '../../lib/xively/models/user';
+import { initialState } from '../blueprint/devices/actions';
 
 export interface AuthorizationState {
   isAuthenticated: boolean;
+  loading: boolean;
+  error: string;
 }
 type Reducer = (state: AuthorizationState, action) => AuthorizationState;
-
-export const reducer: Reducer = (state = { isAuthenticated: false } , action) => {
+const InitialState = {
+  isAuthenticated: false,
+  loading: false,
+  error: undefined,
+}
+export const reducer: Reducer = (state = InitialState , action) => {
   switch (action.type) {
+    case actions.AUTH_LOGIN_REQUEST:
+      return { ...state, loading: true, error: undefined }
     case actions.AUTH_LOGIN_SUCCESS:
-      console.log('HIT AUTH LOGIN REDUCER');
       return { ...state, isAuthenticated: true };
-
+    case actions.AUTH_LOGIN_FAILURE:
+      return { ...state, error: action.payload }
+    case actions.AUTH_LOGOUT_REQUEST:
+      return { ...state, loading: true, error: undefined }
     case actions.AUTH_LOGOUT_SUCCESS:
-      console.log('HIT AUTH ERR REDUCER');
       return { ...state, isAuthenticated: false };
-
+    case actions.AUTH_LOGOUT_FAILURE:
+      return { ...state, error: action.payload }
     default:
       return state;
   }
